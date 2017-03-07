6c5ba6d1a45f4909365c75b1b5b477664a61466f
Adi Zhavo <adi@gram.gs>
2017-03-07 12:00:59 +0000

    Add data class

---
+++ DataBinding/Data.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace DataBinding
+{
+    public class Data<T>
+    {
+        public string branch;
+        public T value;
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder($"\nbranch: {branch}");
+            text.Append($"\nvalue: {value.ToString()}");
+            return text.ToString();
+        }
+    }
+
+    public class PseudoData
+    {
+        public string branch;
+        public string json;
+
+        public override string ToString()
+        {
+            return $"branch: {branch}, json: {json}";
+        }
+    }
+
+    public class DataTypeMap
+    {
+        public string branch;
+        public string type;
+
+        public override string ToString()
+        {
+            return $"branch: {branch}, type: {type}";
+        }
+    }
+}
