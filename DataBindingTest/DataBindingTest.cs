2e9b7562b0cea8eba232c71c4e7edba8b6f7e1f7
Adi Zhavo <adi@gram.gs>
2017-03-20 17:26:03 +0000

    Add initial tests for the data binding service

---
+++ DataBindingTest/DataBindingTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using DataBinding;
+using NUnit.Framework;
+
+namespace DataBindingTest
+{
+    [TestFixture()]
+    public class DataBindingTest
+    {
+        private DataBindingService dataBinding;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dataBinding = new DataBindingService();
+        }
+
+        [Test()]
+        public void TestAdditionOfSingleData()
+        {
+            dataBinding.AddDataNode<bool>("test.bool");
+            Assert.IsTrue(dataBinding.ContainsNode("test.bool"));
+        }
+
+        [Test()]
+        public void TestAdditionOfMultipleData()
+        {
+            dataBinding.AddDataNode<bool>("test.bool");
+            dataBinding.AddDataNode<string>("test.string");
+
+            Assert.IsTrue(dataBinding.ContainsNode("test.bool") && dataBinding.ContainsNode("test.string"));
+        }
+
+        [Test()]
+        public void TestAdditionOfSingleDefaultData()
+        {
+            dataBinding.AddDataNode<bool>("test.bool", true);
+            Assert.IsTrue(dataBinding.ContainsNode("test.bool"));
+        }
+
+        [Test()]
+        public void TestExtractionOfNode()
+        {
+            dataBinding.AddDataNode<bool>("test.bool.true", true);
+            dataBinding.AddDataNode<bool>("test.bool.false");
+
+            Assert.IsTrue(dataBinding.ExtractNode("test.bool.true") != null);
+            Assert.IsTrue(dataBinding.ExtractNode("test.bool.false") != null);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowExceptionIfAddingDataWithoutBranch()
+        {
+            dataBinding.AddDataNode<bool>(string.Empty);
+        }
+    }
+}
